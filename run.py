4b24a319c6a340d149d2021366cb24a535aade09
Conn Warwicker <conn@cmrwarwicker.com>
2020-10-29 14:03:55 +0000

    Fix missing member lists in commands and update settings.dist.json

--- run.py
+++ run.py
@@ -4,12 +4,16 @@ from bot import WriterBot
 from discord.ext import commands
 from pprint import pprint
 
+# As of 1.5 we now need to specify the intent to get the members list, otherwise we can't access it.
+intents = discord.Intents.default()
+intents.members = True
+
 # Load the settings for initial setup
 config = lib.get('./settings.json')
 
 # Load the Bot object
 status = discord.Game( 'Booting up...' )
-bot = WriterBot(command_prefix=WriterBot.load_prefix, activity=status)
+bot = WriterBot(command_prefix=WriterBot.load_prefix, activity=status, intents=intents)
 
 # Load all commands
 bot.load_commands()
