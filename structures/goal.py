b624d4bc59808a135d9908efa7dacc70d2eb4525
Conn Warwicker <conn@cmrwarwicker.com>
2020-11-08 09:53:15 +0000

    Put try/catch around goal reset task, in case of invalid timezone.

--- structures/goal.py
+++ structures/goal.py
@@ -1,4 +1,4 @@
-import lib, time
+import lib, pytz, time
 from structures.db import Database
 from structures.user import User
 
@@ -22,10 +22,12 @@ class Goal:
 
             # Calculate the next reset time for the goal, depending on its type.
             user = User(record['user'], 0)
-            next = user.calculate_user_reset_time(record['type'])
-
-            lib.debug('Setting next ' + record['type'] + ' goal reset time for ' + str(record['user']) + ' to: ' + str(next))
-            self.__db.update('user_goals', {'completed': 0, 'current': 0, 'reset': next}, {'id': record['id']})
+            try:
+                next = user.calculate_user_reset_time(record['type'])
+                lib.debug('Setting next ' + record['type'] + ' goal reset time for ' + str(record['user']) + ' to: ' + str(next))
+                self.__db.update('user_goals', {'completed': 0, 'current': 0, 'reset': next}, {'id': record['id']})
+            except pytz.exceptions.UnknownTimeZoneError:
+                lib.out('[ERROR] Invalid timezone (' + user.get_setting('timezone') + ') for user ' + str(record['user']))
 
         return True
 
