ff674119492dd7efe4bc68696fc1be9c3a0dd57e
Conn Warwicker <conn@cmrwarwicker.com>
2020-12-30 16:53:01 +0000

    Change reassure command to use mentions.
    This removes the need to lookup users, so mentioning can be used to
    reassure other users.

--- cogs/fun/reassure.py
+++ cogs/fun/reassure.py
@@ -1,10 +1,6 @@
 import random, lib, discord, json
 from discord.ext import commands
 
-# debugging
-from pprint import pprint
-from inspect import getmembers
-
 class Reassure(commands.Cog):
 
     def __init__(self, bot):
@@ -26,20 +22,17 @@ class Reassure(commands.Cog):
             mention = context.message.author.mention
         else:
 
-            user = context.guild.get_member_named(who)
+            # We must now mention the user directly, as we can't easily lookup users any more
+            if not who.startswith('<@!') or not who.endswith('>'):
+                return await context.send( lib.get_string('reassure:nomention', guild_id) )
 
-            # If we couldn't find the user, display an error
-            if user is None:
-                await context.send(lib.get_string('err:nouser', guild_id) + ' (There is a known issue with the reassure command, as well as the leaderboards in the Event and XP commands. We are waiting for Discord to respond to an email to get these fixed)')
-                return
-
-            mention = user.mention
+            mention = who
 
         # Load the JSON file with the quotes
-        quotes = lib.get_asset('reassure', guild_id)
+        messages = lib.get_asset('reassure', guild_id)
 
-        max = len(quotes) - 1
-        quote = quotes[random.randint(1, max)]
+        max = len(messages) - 1
+        quote = messages[random.randint(1, max)]
 
         # Send the message
         await context.send( mention + ', ' + format(quote) )
