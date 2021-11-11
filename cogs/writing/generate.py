ca1a107619bd43a8a9947cb06e99d1249a44685c
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2021-11-11 20:06:36 +0200

    migrate generate to slash

--- cogs/writing/generate.py
+++ cogs/writing/generate.py
@@ -1,37 +1,70 @@
-import discord, lib, time
+import lib
+import time
 from discord.ext import commands
 from structures.generator import NameGenerator
 from structures.user import User
 from structures.wrapper import CommandWrapper
 from structures.guild import Guild
+from discord_slash import cog_ext, SlashContext
+from discord_slash.model import SlashCommandOptionType
+from discord_slash.utils.manage_commands import create_option, create_choice
+
+SUPPORTED_TYPES = {
+    'char': 'Character',
+    'place': 'Place',
+    'land': 'Land',
+    'idea': 'Idea',
+    'book': 'Book',
+    'book_fantasy': 'Fantasy Book',
+    'book_horror': 'Horror Book',
+    'book_hp': 'Harry Potter Book',
+    'book_mystery': 'Mystery Book',
+    'book_rom': 'Romance Book',
+    'book_sf': 'Sci-Fi Book',
+    'prompt': 'Prompt',
+    'face': 'Face'
+}
 
 class Generate(commands.Cog, CommandWrapper):
 
     def __init__(self, bot):
         self.bot = bot
-        self._supported_types = ['char', 'place', 'land', 'idea', 'book', 'book_fantasy', 'book_horror', 'book_hp', 'book_mystery', 'book_rom', 'book_sf', 'prompt', 'face']
         self._urls = {
             'face': 'https://thispersondoesnotexist.com/image'
         }
-        self._arguments = [
-            {
-                'key': 'type',
-                'prompt': 'generate:argument:type',
-                'required': True,
-                'check': lambda content : content in self._supported_types,
-                'error': 'generate:err:type'
-            },
-            {
-                'key': 'amount',
-                'required': False,
-                'prompt': 'generate:argument:amount',
-                'type': int
-            }
-        ]
 
-    @commands.command(name="generate")
-    @commands.guild_only()
-    async def generate(self, context, type=None, amount=None):
+    @cog_ext.cog_slash(
+        name="generate",
+        description="Random generator for character names, place names, land names, book titles, story ideas, prompts.",
+        options=[
+            create_option(
+                name="type",
+                description="What to generate",
+                required=True,
+                option_type=SlashCommandOptionType.STRING,
+                choices=[
+                    create_choice(
+                        name=name,
+                        value=value
+                    ) for value, name in SUPPORTED_TYPES.items()
+                ]
+            ),
+            create_option(
+                name="amount",
+                description="How many items to generate",
+                required=False,
+                option_type=SlashCommandOptionType.INTEGER
+            ),
+            create_option(
+                name="hidden",
+                description="Should the response be in a hidden message?",
+                option_type=SlashCommandOptionType.BOOLEAN,
+                required=False
+            )
+        ]
+    )
+    async def generate(self, context: SlashContext, type: str, amount: int = None,
+                       hidden: bool = False):
         """
         Random generator for various things (character names, place names, land names, book titles, story ideas, prompts).
         Define the type of item you wanted generated and then optionally, the amount of items to generate.
@@ -51,22 +84,17 @@ class Generate(commands.Cog, CommandWrapper):
             !generate prompt - generates a story prompt
             !generate face - generates a random person's face
         """
+        # send the ACK message in case of delays
+        await context.defer(hidden=hidden)
+
         if not Guild(context.guild).is_command_enabled('generate'):
             return await context.send(lib.get_string('err:disabled', context.guild.id))
 
-        user = User(context.message.author.id, context.guild.id, context)
+        user = User(context.author_id, context.guild.id, context)
 
         # If no amount specified, use the default
         if amount is None:
             amount = NameGenerator.DEFAULT_AMOUNT
-
-        # Check the arguments are valid
-        args = await self.check_arguments(context, type=type, amount=amount)
-        if not args:
-            return
-
-        type = args['type'].lower()
-        amount = int(args['amount'])
 
         # For faces, we want to just call an API url.
         if type == 'face':
@@ -84,7 +112,10 @@ class Generate(commands.Cog, CommandWrapper):
 
         return await context.send(user.get_mention() + ', ' + results['message'] + names)
 
-
+    @commands.command(name="generate")
+    @commands.guild_only()
+    async def old(self, context):
+        await context.send(lib.get_string('err:slash', context.guild.id))
 
 def setup(bot):
     bot.add_cog(Generate(bot))
