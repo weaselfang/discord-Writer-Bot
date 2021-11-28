15fcb20cc2d553289bc8e0ad7da51e74a03b599a
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2021-11-28 14:59:37 +0200

    base slash support (#108)

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 import discord, json, lib
 from bot import WriterBot
 from discord.ext import commands
+from discord_slash import SlashCommand
 
 # Load the settings for initial setup
 config = lib.get('./settings.json')
@@ -9,6 +10,7 @@ config = lib.get('./settings.json')
 # Load the Bot object
 status = discord.Game( 'Booting up...' )
 bot = WriterBot(command_prefix=WriterBot.load_prefix, activity=status)
+slash = SlashCommand(bot, sync_commands=True)
 
 # Load all commands
 bot.load_commands()
