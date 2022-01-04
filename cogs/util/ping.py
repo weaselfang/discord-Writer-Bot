aced495cf3257b705deaeb41059fc060fb1e6ce8
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2022-01-04 12:41:47 +0200

    slash ping

--- cogs/util/ping.py
+++ cogs/util/ping.py
@@ -1,15 +1,16 @@
-import discord, lib
-from discord.ext import commands
+import lib
 from structures.guild import Guild
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+
 
 class Ping(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="ping")
-    @commands.guild_only()
-    async def ping(self, context):
+    @cog_ext.cog_slash(name="ping", description="Displays latency between client and bot")
+    async def ping(self, context: SlashContext):
         """
         Displays latency between client and bot
         """
@@ -18,6 +19,11 @@ class Ping(commands.Cog):
 
         latency = round(self.bot.latency * 1000, 2)
         return await context.send('Pong! ' + str(latency) + 'ms')
+
+    @commands.command(name="ping")
+    @commands.guild_only()
+    async def old_ping(self, context):
+        await context.send(lib.get_string('err:slash', context.guild.id))
 
 
 def setup(bot):
