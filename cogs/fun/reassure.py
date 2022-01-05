2436511a041d4e30c4f68c4f3e3d0aec92899718
Conn Warwicker <conn@cmrwarwicker.com>
2022-01-05 18:13:29 +0000

    Migrate `reassure` to slash command

--- cogs/fun/reassure.py
+++ cogs/fun/reassure.py
@@ -1,7 +1,9 @@
-import random, lib, discord, json
+import random, lib, json
 from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+from discord_slash.model import SlashCommandOptionType
+from discord_slash.utils.manage_commands import create_option
 from structures.guild import Guild
-
 class Reassure(commands.Cog):
 
     def __init__(self, bot):
@@ -9,35 +11,53 @@ class Reassure(commands.Cog):
 
     @commands.command(name="reassure")
     @commands.guild_only()
-    async def reassure(self, context, who=None):
+    async def old(self, context):
+        await context.send(lib.get_string('err:slash', context.guild.id))
+
+    @cog_ext.cog_slash(name="reassure",
+                       description="Send a random reassuring message to a user or yourself",
+                       options=[
+                           create_option(name="who",
+                                         description="Who do you want to reassure?",
+                                         option_type=SlashCommandOptionType.STRING,
+                                         required=False)
+                       ])
+    async def reassure(self, context: SlashContext, who: str = None):
         """
         Reassures you that everything will be okay.
 
-        Examples: !reassure
+        :param SlashContext context: SlashContext object
+        :param str|None who: The name of the user to reassure
+        :rtype: void
         """
+        # Send "bot is thinking" message, to avoid failed commands if latency is high.
+        await context.defer()
+
+        # Make sure the guild has this command enabled.
         if not Guild(context.guild).is_command_enabled('reassure'):
-            return await context.send(lib.get_string('err:disabled', context.guild.id))
+            return await context.send(lib.get_string('err:disabled', context.guild_id))
 
-        guild_id = context.guild.id
+        guild_id = context.guild_id
 
-        # If no name passed through, default to the author of the command
+        # If no name passed through, default to the author of the command.
         if who is None:
-            mention = context.message.author.mention
+            mention = context.author.mention
         else:
 
-            # We must now mention the user directly, as we can't easily lookup users any more
+            # We must now mention the user directly, as we can't easily lookup users any more.
             if not who.startswith('<@!') or not who.endswith('>'):
                 return await context.send( lib.get_string('reassure:nomention', guild_id) )
 
             mention = who
 
-        # Load the JSON file with the quotes
+        # Load the JSON file with the quotes.
         messages = lib.get_asset('reassure', guild_id)
 
+        # Pick a random message.
         max = len(messages) - 1
         quote = messages[random.randint(1, max)]
 
-        # Send the message
+        # Send the message.
         await context.send( mention + ', ' + format(quote) )
 
 def setup(bot):
