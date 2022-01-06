b0fdd55bb8d3212f9c16464763474cc81dcd383a
Conn Warwicker <conn@cmrwarwicker.com>
2022-01-06 18:54:18 +0000

    Migrate `profile` to slash command

--- cogs/util/profile.py
+++ cogs/util/profile.py
@@ -1,5 +1,6 @@
 import discord, lib
 from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
 from structures.db import Database
 from structures.user import User
 from structures.wrapper import CommandWrapper
@@ -13,18 +14,27 @@ class Profile(commands.Cog, CommandWrapper):
 
     @commands.command(name="profile")
     @commands.guild_only()
-    async def profile(self, context):
+    async def old(self, context):
+        await context.send(lib.get_string('err:slash', context.guild.id))
+
+    @cog_ext.cog_slash(name="profile",
+                       description="Display your Writer-Bot profile information and statistics")
+    async def profile(self, context: SlashContext):
         """
         Displays your Writer-Bot profile information and statistics.
+
+        :param SlashContext context: SlashContext object
+        :rtype: void
         """
 
-        if not Guild(context.guild).is_command_enabled('profile'):
-            return await context.send(lib.get_string('err:disabled', context.guild.id))
+        # Send "bot is thinking" message, to avoid failed commands if latency is high.
+        await context.defer()
 
-        user = User(context.message.author.id, context.guild.id, context)
-        goals = {
-            'daily': user.get_goal_progress('daily')
-        }
+        # Make sure the guild has this command enabled.
+        if not Guild(context.guild).is_command_enabled('profile'):
+            return await context.send(lib.get_string('err:disabled', context.guild_id))
+
+        user = User(context.author.id, context.guild.id, context)
         profile = {
             'lvlxp': user.get_xp_bar(),
             'words': user.get_stat('total_words_written'),
@@ -53,8 +63,7 @@ class Profile(commands.Cog, CommandWrapper):
         embed.add_field(name=lib.get_string('profile:monthlygoalscompleted', user.get_guild()), value=profile['monthly_goals_completed'], inline=True)
         embed.add_field(name=lib.get_string('profile:yearlygoalscompleted', user.get_guild()), value=profile['yearly_goals_completed'], inline=True)
 
-
-        # Send the message
+        # Send the embedded message
         await context.send(embed=embed)
 
 
