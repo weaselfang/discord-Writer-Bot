52a0356bccce35abea904c71b65cf88de3c48c19
Conn Warwicker <conn@cmrwarwicker.com>
2022-03-30 21:25:49 +0100

    Migrate `goal` to slash command.

--- cogs/writing/goal.py
+++ cogs/writing/goal.py
@@ -1,73 +1,204 @@
 import discord, lib, time, math
 from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+from discord_slash.model import SlashCommandOptionType
+from discord_slash.utils.manage_commands import create_option, create_choice
 from structures.db import Database
 from structures.user import User
-from structures.wrapper import CommandWrapper
 from structures.guild import Guild
 
-class Goal(commands.Cog, CommandWrapper):
+GOAL_TYPE_DAILY = 'daily'
+GOAL_TYPE_WEEKLY = 'weekly'
+GOAL_TYPE_MONTHLY = 'monthly'
+GOAL_TYPE_YEARLY = 'yearly'
 
-    GOAL_TYPE_DAILY = 'daily'
-    GOAL_TYPE_WEEKLY = 'weekly'
-    GOAL_TYPE_MONTHLY = 'monthly'
-    GOAL_TYPE_YEARLY = 'yearly'
+GOAL_TYPES = [GOAL_TYPE_DAILY, GOAL_TYPE_WEEKLY, GOAL_TYPE_MONTHLY, GOAL_TYPE_YEARLY]
+
+class Goal(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
         self.__db = Database.instance()
-        self._arguments = [
-            {
-                'key': 'option',
-                'prompt': 'goal:argument:option',
-                'required': True
-            },
-            {
-                'key': 'value',
-                'required': False
-            }
-        ]
-        self.types = [self.GOAL_TYPE_DAILY, self.GOAL_TYPE_WEEKLY, self.GOAL_TYPE_MONTHLY, self.GOAL_TYPE_YEARLY]
 
-    @commands.command(name="goal", aliases=['goals'])
+    @commands.command(name="goal")
     @commands.guild_only()
-    async def goal(self, context, option=None, type=None, value=None):
+    async def old(self, context):
         """
-        Sets a daily goal which resets every 24 hours at midnight in your timezone.
+        Migrated command, so just display a message for now.
+        :param context: Discord context
+        """
+        return await context.send(lib.get_string('err:slash', context.guild.id))
 
-        Examples:
-            !goal - Print a table of all your goals, with their progress and next reset time.
-            !goal check daily - Checks how close you are to your daily goal
-            !goal set weekly 500 - Sets your weekly goal to be 500 words per day
-            !goal cancel monthly - Deletes your monthly goal
-            !goal time daily - Checks how long until your daily goal resets
+    async def pre_command_checks(self, context: SlashContext):
         """
+        Run pre-command checks, to avoid duplication of code.
+        @return:
+        """
+        # Send "bot is thinking" message, to avoid failed commands if latency is high.
+        await context.defer()
+
+        # Make sure guild has this command enabled.
         if not Guild(context.guild).is_command_enabled('goal'):
-            return await context.send(lib.get_string('err:disabled', context.guild.id))
+            await context.send(lib.get_string('err:disabled', context.guild_id))
+            return False
 
-        user = User(context.message.author.id, context.guild.id, context)
+        return True
 
-        # If no option is sent and we just do `goal` then display a table of all their goals.
-        if option is None:
-            return await self.run_check_all(context)
+    @cog_ext.cog_subcommand(
+        base="goal",
+        name="set",
+        description="Set a writing goal",
+        options=[
+            create_option(
+                name="type",
+                description="Type of goal to set",
+                option_type=SlashCommandOptionType.STRING,
+                required=True,
+                choices=[
+                    create_choice(name=value, value=value) for value in GOAL_TYPES
+                ]
+            ),
+            create_option(
+                name="value",
+                description="Word count to set for the goal",
+                option_type=SlashCommandOptionType.INTEGER,
+                required=True
+            )
+        ]
+    )
+    async def goal_set(self, context: SlashContext, type: str, value: int):
 
-        # Otherwise, we must specify a type.
-        if type is None or type not in self.types:
-            return await context.send(user.get_mention() + ', ' + lib.get_string('goal:invalidtype', user.get_guild()))
+        # Run the pre-command checks.
+        if await self.pre_command_checks(context) is False:
+            return
 
-        if option == 'set':
-            return await self.run_set(context, type, value)
-        elif option == 'cancel' or option == 'delete' or option == 'reset':
-            return await self.run_cancel(context, type)
-        elif option == 'time':
-            return await self.run_time(context, type)
-        elif option == 'check':
-            return await self.run_check(context, type)
-        elif option == 'history':
-            return await self.run_history(context, type)
-        elif option == 'update':
-            return await self.run_update(context, type, value)
-        else:
-            return await context.send(user.get_mention() + ', ' + lib.get_string('goal:invalidoption', user.get_guild()))
+        return await self.run_set(context, type, value)
+
+    @cog_ext.cog_subcommand(
+        base="goal",
+        name="check",
+        description="Check one or all of your writing goals",
+        options=[
+            create_option(
+                name="type",
+                description="Type of goal to check. If not specified, will check all goals.",
+                option_type=SlashCommandOptionType.STRING,
+                required=False,
+                choices=[
+                    create_choice(name=value, value=value) for value in GOAL_TYPES
+                ]
+            )
+        ]
+    )
+    async def goal_check(self, context: SlashContext, type: str = None):
+
+        # Run the pre-command checks.
+        if await self.pre_command_checks(context) is False:
+            return
+
+        return await self.run_check(context, type)
+
+    @cog_ext.cog_subcommand(
+        base="goal",
+        name="delete",
+        description="Delete one of your writing goals",
+        options=[
+            create_option(
+                name="type",
+                description="Type of goal to delete",
+                option_type=SlashCommandOptionType.STRING,
+                required=True,
+                choices=[
+                    create_choice(name=value, value=value) for value in GOAL_TYPES
+                ]
+            )
+        ]
+    )
+    async def goal_delete(self, context: SlashContext, type: str):
+
+        # Run the pre-command checks.
+        if await self.pre_command_checks(context) is False:
+            return
+
+        return await self.run_cancel(context, type)
+
+    @cog_ext.cog_subcommand(
+        base="goal",
+        name="time",
+        description="Check the time left for one of your writing goals",
+        options=[
+            create_option(
+                name="type",
+                description="Type of goal to check",
+                option_type=SlashCommandOptionType.STRING,
+                required=True,
+                choices=[
+                    create_choice(name=value, value=value) for value in GOAL_TYPES
+                ]
+            )
+        ]
+    )
+    async def goal_time(self, context: SlashContext, type: str):
+
+        # Run the pre-command checks.
+        if await self.pre_command_checks(context) is False:
+            return
+
+        return await self.run_time(context, type)
+
+    @cog_ext.cog_subcommand(
+        base="goal",
+        name="update",
+        description="Update the word count value of a writing goal, without affecting your XP or other goals.",
+        options=[
+            create_option(
+                name="type",
+                description="Type of goal to set",
+                option_type=SlashCommandOptionType.STRING,
+                required=True,
+                choices=[
+                    create_choice(name=value, value=value) for value in GOAL_TYPES
+                ]
+            ),
+            create_option(
+                name="value",
+                description="Word count to set for the goal",
+                option_type=SlashCommandOptionType.INTEGER,
+                required=True
+            )
+        ]
+    )
+    async def goal_update(self, context: SlashContext, type: str, value: int):
+
+        # Run the pre-command checks.
+        if await self.pre_command_checks(context) is False:
+            return
+
+        return await self.run_update(context, type, value)
+
+    @cog_ext.cog_subcommand(
+        base="goal",
+        name="history",
+        description="Check your your history for one of your writing goals",
+        options=[
+            create_option(
+                name="type",
+                description="Type of goal to check the history of",
+                option_type=SlashCommandOptionType.STRING,
+                required=True,
+                choices=[
+                    create_choice(name=value, value=value) for value in GOAL_TYPES
+                ]
+            )
+        ]
+    )
+    async def goal_history(self, context: SlashContext, type: str):
+
+        # Run the pre-command checks.
+        if await self.pre_command_checks(context) is False:
+            return
+
+        return await self.run_history(context, type)
 
     async def run_update(self, context, type, amount):
         """
@@ -78,7 +209,8 @@ class Goal(commands.Cog, CommandWrapper):
         @param amount:
         @return:
         """
-        user = User(context.message.author.id, context.guild.id, context)
+        user = User(context.author_id, context.guild_id, context)
+
         type_string = lib.get_string('goal:' + type, user.get_guild())
         user_goal = user.get_goal(type)
 
@@ -100,7 +232,8 @@ class Goal(commands.Cog, CommandWrapper):
         @param type:
         @return:
         """
-        user = User(context.message.author.id, context.guild.id, context)
+        user = User(context.author_id, context.guild_id, context)
+
         type_string = lib.get_string('goal:' + type, user.get_guild()).lower()
         history = user.get_goal_history(type)
 
@@ -125,14 +258,16 @@ class Goal(commands.Cog, CommandWrapper):
         """
 
         now = int(time.time())
-        user = User(context.message.author.id, context.guild.id, context)
+        user = User(context.author_id, context.guild_id, context)
         embed = discord.Embed(title=lib.get_string('goals', user.get_guild()), color=10038562)
 
-        for type in self.types:
+        for type in GOAL_TYPES:
 
             type_string = lib.get_string('goal:'+type, user.get_guild())
             goal = user.get_goal(type)
+
             if goal is not None:
+
                 progress = user.get_goal_progress(type)
                 seconds_left = goal['reset'] - now
                 left = lib.secs_to_days(seconds_left)
@@ -147,7 +282,8 @@ class Goal(commands.Cog, CommandWrapper):
                 text = lib.get_string('goal:yourgoal', user.get_guild()).format(type_string, goal['goal']) +  "\n"
                 text += lib.get_string('goal:status', user.get_guild()).format(progress['percent'], type_string, current_wordcount, goal_wordcount) + "\n"
                 text += lib.get_string('goal:timeleft', user.get_guild()).format(lib.format_secs_to_days(seconds_left), type_string)
-                if type != self.GOAL_TYPE_DAILY:
+
+                if type != GOAL_TYPE_DAILY:
                     days = left['days']
                     # if someone has, for instance, 3 days 2 hours, count that as 4 days (remainder of today + 3 days)
                     hours = left['hours']
@@ -155,9 +291,11 @@ class Goal(commands.Cog, CommandWrapper):
                     if words_remaining > 0:
                         average_wordcount_needed = math.ceil(words_remaining / (1 if days == 0 else days))
                         text += "\n" + lib.get_string('goal:rate', user.get_guild()).format(average_wordcount_needed, type_string)
+
                 if words_remaining == 0:
                     # They met their goal!
                     text += "\n" + lib.get_string('goal:met:noxp', user.get_guild()).format(type_string, goal_wordcount)
+
             else:
                 text = None
 
@@ -165,7 +303,7 @@ class Goal(commands.Cog, CommandWrapper):
 
         # Send the message
         await context.send(embed=embed)
-
+    #
     async def run_time(self, context, type):
         """
         Check how long until the goal resets
@@ -173,7 +311,7 @@ class Goal(commands.Cog, CommandWrapper):
         :param type:
         :return:
         """
-        user = User(context.message.author.id, context.guild.id, context)
+        user = User(context.author_id, context.guild_id, context)
 
         # Get the goal of this type for this user
         goal = user.get_goal(type)
@@ -187,17 +325,16 @@ class Goal(commands.Cog, CommandWrapper):
         else:
             return await context.send(user.get_mention() + ', ' + lib.get_string('goal:nogoal', user.get_guild()).format(type, type))
 
-
-
     async def run_cancel(self, context, type):
 
-        user = User(context.message.author.id, context.guild.id, context)
+        user = User(context.author_id, context.guild_id, context)
+
         user.delete_goal(type)
-        return await context.send(user.get_mention() + ', ' + lib.get_string('goal:givenup', user.get_guild()))
+        return await context.send(user.get_mention() + ', ' + lib.get_string('goal:givenup', user.get_guild()).format(type))
 
     async def run_set(self, context, type, amount):
 
-        user = User(context.message.author.id, context.guild.id, context)
+        user = User(context.author_id, context.guild_id, context)
 
         # Check if we can convert the amount to an int
         amount = lib.is_number(amount)
@@ -211,9 +348,13 @@ class Goal(commands.Cog, CommandWrapper):
         reset_every = lib.get_string('goal:set:'+type, user.get_guild())
         return await context.send(user.get_mention() + ', ' + lib.get_string('goal:set', user.get_guild()).format(type, amount, reset_every, timezone))
 
-    async def run_check(self, context, type):
+    async def run_check(self, context, type = None):
 
-        user = User(context.message.author.id, context.guild.id, context)
+        if type is None:
+            return await self.run_check_all(context)
+
+        user = User(context.author_id, context.guild_id, context)
+
         type_string = lib.get_string('goal:' + type, user.get_guild())
 
         user_goal = user.get_goal(type)
